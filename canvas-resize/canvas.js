b765b9937554eab0bae184c71f4af55cac125fb1
Satyum <satyumspathania@gmail.com>
2023-10-16 15:51:26 +0530

    drawing on html5 canvas

--- canvas-resize/canvas.js
+++ canvas-resize/canvas.js
@@ -4,5 +4,28 @@ canvas.height = window.innerHeight;
 
 var c = canvas.getContext("2d");
 
+c.fillStyle = "rgba(255, 0, 0, 0.3)";
 c.fillRect(100, 100, 100, 100);
+c.fillStyle = "rgba(0, 0, 255, 0.3)";
+c.fillRect(400, 400, 100, 100);
+c.fillStyle = "rgba(0, 255, 0, 0.3)";
+c.fillRect(300, 200, 100, 100);
 console.log(canvas);
+
+//Line
+c.beginPath();
+c.moveTo(50, 300);
+c.lineTo(300, 100);
+c.lineTo(400, 300);
+c.strokeStyle = "red";
+c.stroke();
+
+// Arc/Circle
+for (var i = 0; i < 200; i++) {
+  var x = Math.random() * window.innerWidth;
+  var y = Math.random() * window.innerHeight;
+  c.beginPath();
+  c.arc(x, y, 30, 0, Math.PI * 2, false);
+  c.strokeStyle = "blue";
+  c.stroke();
+}
