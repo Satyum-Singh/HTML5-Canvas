5fa071bc44401918f5a53e090d0fca1338313a5e
Satyum <satyumspathania@gmail.com>
2023-10-17 10:01:58 +0530

    Learned about interactive canvas.

--- canvas-resize/canvas.js
+++ canvas-resize/canvas.js
@@ -30,18 +30,39 @@ var c = canvas.getContext("2d");
 //   c.stroke();
 // }
 
+var mouse = {
+  x: undefined,
+  y: undefined,
+};
+
+var maxRadius = 40;
+var minRadius = 2;
+
+var colorArray = ["#2C3E50", "#E74C3C", "#ECF0F1", "#3498DB", "#2980B9"];
+
+window.addEventListener("mousemove", function (event) {
+  mouse.x = event.x;
+  mouse.y = event.y;
+});
+
+window.addEventListener("resize", function () {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+});
+
 function Circle(x, y, dx, dy, radius) {
   this.x = x;
   this.y = y;
   this.dx = dx;
   this.dy = dy;
   this.radius = radius;
+  this.minRadius = radius;
+  this.color = colorArray[Math.floor(Math.random() * colorArray.length)];
 
   this.draw = function () {
     c.beginPath();
     c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
-    c.strokeStyle = "yellow";
-    c.stroke();
+    c.fillStyle = this.color;
     c.fill();
   };
 
@@ -53,17 +74,37 @@ function Circle(x, y, dx, dy, radius) {
     }
     this.x += this.dx;
     this.y += this.dy;
+
+    //interactivity
+    if (
+      mouse.x - this.x < 50 &&
+      mouse.x - this.x > -50 &&
+      mouse.y - this.y < 50 &&
+      mouse.y - this.y > -50
+    ) {
+      if (this.radius < maxRadius) {
+        this.radius += 3;
+      }
+    } else if (this.radius > 5) {
+      this.radius -= 3;
+    }
+
     this.draw();
   };
 }
 
 var circleArray = [];
-for (var i = 0; i < 100; i++) {
-  var x = Math.random() * (innerWidth - 50 * 2) + 50;
-  var y = Math.random() * (innerHeight - 50 * 2) + 50;
-  var dx = Math.random() - 0.5 * 10;
-  var dy = Math.random() - 0.5 * 10;
-  circleArray.push(new Circle(x, y, dx, dy, 50));
+
+function init() {
+  circleArray = [];
+  for (var i = 0; i < 1200; i++) {
+    var radius = Math.random() * 3 + 1;
+    var x = Math.random() * (innerWidth - radius * 2) + radius;
+    var y = Math.random() * (innerHeight - radius * 2) + radius;
+    var dx = Math.random() - 0.5 * 5;
+    var dy = Math.random() - 0.5 * 5;
+    circleArray.push(new Circle(x, y, dx, dy, radius));
+  }
 }
 
 function animate() {
@@ -74,4 +115,5 @@ function animate() {
   }
 }
 
+init();
 animate();
