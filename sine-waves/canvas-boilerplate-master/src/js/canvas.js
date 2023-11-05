4e4058bdbe264b4a73cde97c43505d27d7786da9
Satyum <satyumspathania@gmail.com>
2023-11-05 14:31:27 +0530

    adding dat.gui

--- sine-waves/canvas-boilerplate-master/src/js/canvas.js
+++ sine-waves/canvas-boilerplate-master/src/js/canvas.js
@@ -1,6 +1,18 @@
+import * as dat from 'dat.gui'
+
+const dat = new dat.GUI()
+
 const canvas = document.querySelector('canvas')
 const c = canvas.getContext('2d')
 
-canvas.width = innerWidth
-canvas.height = innerHeight
+canvas.width = window.innerWidth
+canvas.height = window.innerHeight
 
+c.beginPath();
+
+c.moveTo(0,canvas.height/2);
+
+for(let i = 0; i < canvas.width; i++){
+  c.lineTo(i, canvas.height/2 + Math.sin(i*0.01)*100);
+}
+c.stroke();
