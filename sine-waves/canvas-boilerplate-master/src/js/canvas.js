ba38b4ecf0b33c5ec6f330c4507e06e3ff82f546
Satyum <satyumspathania@gmail.com>
2023-11-05 15:39:42 +0530

    fixing code.

--- sine-waves/canvas-boilerplate-master/src/js/canvas.js
+++ sine-waves/canvas-boilerplate-master/src/js/canvas.js
@@ -1,18 +1,27 @@
-import * as dat from 'dat.gui'
+import * as dat from "dat.gui";
 
-const dat = new dat.GUI()
+const gui = new dat.GUI();
+const canvas = document.querySelector("canvas");
+const c = canvas.getContext("2d");
 
-const canvas = document.querySelector('canvas')
-const c = canvas.getContext('2d')
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
 
-canvas.width = window.innerWidth
-canvas.height = window.innerHeight
+const wave = {
+  y: canvas.heigth / 2,
+  length: 0.01,
+  amplitude: 100,
+};
+
+gui.add(wave, "y", 0, canvas.height);
+gui.add(wave, "length", -0.01, 0.01);
+gui.add(wave, "amplitude", -300, 300);
 
 c.beginPath();
 
-c.moveTo(0,canvas.height/2);
+c.moveTo(0, canvas.height / 2);
 
-for(let i = 0; i < canvas.width; i++){
-  c.lineTo(i, canvas.height/2 + Math.sin(i*0.01)*100);
+for (let i = 0; i < canvas.width; i++) {
+  c.lineTo(i, canvas.height / 2 + Math.sin(i * 0.01) * 100);
 }
 c.stroke();
