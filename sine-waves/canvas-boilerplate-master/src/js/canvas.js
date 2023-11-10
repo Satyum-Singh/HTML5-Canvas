aaddac376232a97375cfd1ff6baf908c916605ce
Satyum <satyumspathania@gmail.com>
2023-11-10 21:39:25 +0530

    Binded utils.js with canvas.js

--- sine-waves/canvas-boilerplate-master/src/js/canvas.js
+++ sine-waves/canvas-boilerplate-master/src/js/canvas.js
@@ -8,6 +8,21 @@ const d = canvas.getContext("2d");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
+function randomIntFromRange(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min)
+}
+
+function randomColor(colors) {
+  return colors[Math.floor(Math.random() * colors.length)]
+}
+
+function distance(x1, y1, x2, y2) {
+  const xDist = x2 - x1
+  const yDist = y2 - y1
+
+  return Math.sqrt(Math.pow(xDist, 2) + Math.pow(yDist, 2))
+}
+
 const wave = {
   y: canvas.heigth / 2,
   length: 0.01,
